c030e3a038840db8306c3eda122ba6de2bceba6c
Nidhi <ojhanidhi036@gmail.com>
2024-05-11 16:41:37 +0530

    Destini Project done

---
+++ DestiniProject/Model/Story.swift
@@ -0,0 +1,18 @@
+//
+//  Story.swift
+//  DestiniProject
+//
+//  Created by Nidhi on 05/05/24.
+//
+
+import Foundation
+
+struct Story {
+    
+    let title:String
+    let choice1:String
+    let choice1Destination:Int
+    let choice2:String
+    let choice2Destination: Int
+    
+}
